d086852cf95d7103f2bd0784370d5fed833aacf2
Nizmox <nizmox@gmail.com>
2014-06-30 18:26:10 +1000

    simon reworked

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-// /*Run this file to serve the index.html file via the Node server*/
-// var http = require('http'),
-//     fs = require('fs');
-// fs.readFile('public/html/index.html', function (err, html) {
-//     if (err) {
-//         throw err; 
-//     }       
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"}); 
-//         response.write(html);  
-//         response.end();  
-//     }).listen(3000);
-// });
-
-
+//require modules
 var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    app = express();
+    path = require('path');
 
-// var Leap = require('leapjs');
-// var spheron = require('spheron');
+//create web server
+var app = express();
+
+
+//require the module
+var test = require('./node_modules/charlie');
+test();
 
 // Remember: The order of the middleware matters!
 
 // Everything in public will be accessible from '/'
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Everything in 'vendor/thoughtbrain' will be "mounted" in '/public'
-// app.use('/public', express.static(path.join(__dirname, 'vendor/thoughtbrain')));
-
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.all('*', function(req, res){
-  res.sendfile('views/index.html')
+  res.sendfile('views/index.html');
 });
 
-http.createServer(app).listen(3000);
+app.listen(3001);
+console.log("server running on port 3001");
