160990a6a3c89c7d6b2c29de7d02444a07345248
Charlie <annecharlotte.gerard@gmail.com>
2014-06-30 15:55:07 +1000

    organise files

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-/*Run this file to serve the index.html file via the Node server*/
-var http = require('http'),
-    fs = require('fs');
-fs.readFile('html/index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"}); 
-        response.write(html);  
-        response.end();  
-    }).listen(3000);
+// /*Run this file to serve the index.html file via the Node server*/
+// var http = require('http'),
+//     fs = require('fs');
+// fs.readFile('public/html/index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"}); 
+//         response.write(html);  
+//         response.end();  
+//     }).listen(3000);
+// });
+
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    app = express();
+
+// var Leap = require('leapjs');
+// var spheron = require('spheron');
+
+// Remember: The order of the middleware matters!
+
+// Everything in public will be accessible from '/'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Everything in 'vendor/thoughtbrain' will be "mounted" in '/public'
+// app.use('/public', express.static(path.join(__dirname, 'vendor/thoughtbrain')));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.all('*', function(req, res){
+  res.sendfile('views/index.html')
 });
 
-
+http.createServer(app).listen(3000);
